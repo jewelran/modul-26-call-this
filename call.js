11840575e9c1ae34e43dfd63758c52116eeb61ee
jewelran <mdjewelrana6487@gmail.com>
2020-08-11 21:40:51 +0600

    new

---
+++ call.js
@@ -0,0 +1,8 @@
+const normalPerson ={
+    firstName : "jewel",
+    lastName : "rana",
+    salary :12000,
+    getFullName: function(){
+        console.log(this.firstName,this.lastName);
+    }
+}
